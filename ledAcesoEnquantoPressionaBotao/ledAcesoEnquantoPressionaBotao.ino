fc8fab4759dda7565825dc51269fa90c20230b02
gi-armani <giovana_armani@usp.br>
2020-10-05 22:57:50 -0300

    Circuito do led que fica aceso enquanto voce segura o botao

---
+++ ledAcesoEnquantoPressionaBotao/ledAcesoEnquantoPressionaBotao.ino
@@ -0,0 +1,17 @@
+int botao = 9;
+int led = 7;
+ 
+void setup()
+{
+  pinMode(botao, INPUT_PULLUP); // define o pino do botao como entrada "INPUT"
+  pinMode(led, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+ 
+void loop()
+{
+  while(digitalRead(botao) == LOW) { // Se o botão for pressionado
+    digitalWrite(led, HIGH); 
+  }   
+  digitalWrite(led, LOW); 
+}
